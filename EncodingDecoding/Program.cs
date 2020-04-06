8b67f4c98011ce70ffd229baf9e7ea998836c98d
hakiki19 <mdl.hqq@gmail.com>
2020-04-06 10:42:02 +0700

    commit pertama

--- EncodingDecoding/Program.cs
+++ EncodingDecoding/Program.cs
@@ -10,6 +10,24 @@ namespace EncodingDecoding
     {
         static void Main(string[] args)
         {
+            List<string> data =new List<string>() { "@P", "AQ", "BR", "CS", "DT", "EU", "FV", "GW", "HX", "IY", "JZ", "K[", "L'\'", "M]", "N^", "O_"};
+            string input = "saya_mau_Makan";
+            string encode = "";
+            string output ="";
+            char[] cInput = input.ToCharArray();
+            foreach (char i in cInput)
+            {
+                string value = i.ToString().ToUpper();
+                List<string> nilai = data.FindAll(x => x.Contains(value));
+                foreach(string a in nilai)
+                {
+                    int index = a.IndexOf(value);
+                    encode=a.Remove(index,1);
+                    output += encode;
+                }                
+            }
+            Console.WriteLine("output {0}", output);
+            Console.ReadKey();
         }
     }
 }
